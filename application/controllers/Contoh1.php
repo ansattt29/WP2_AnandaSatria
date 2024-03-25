9cea4414bd7b85abb0e8c087eeb8305049d96787
ansattt29 <anandasatriaa03@gmail.com>
2024-03-25 23:29:26 +0700

    first commit

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Ananda Satria
+        Saya tinggal di daerah Bekasi Timur
+        olah raga yang saya sukai adalah
+        Badminton";
+    }
+}
