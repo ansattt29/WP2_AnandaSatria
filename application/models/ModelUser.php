e8b071388a02e9125916e622e07a600cae674e18
ansattt29 <anandasatriaa03@gmail.com>
2024-07-02 11:03:50 +0700

    Tugas Pertemuan 9

--- application/models/ModelUser.php
+++ application/models/ModelUser.php
@@ -32,5 +32,25 @@ class ModelUser extends CI_Model
  {
     return $this->db->get_where('user', $where);
  }
+ public function getUserWhere($where = null)
+ {
+    return $this->db->get_where('user', $where);
+    }
+    public function cekUserAccess($where = null)
+    {
+    $this->db->select('*');
+    $this->db->from('access_menu');
+    $this->db->where($where);
+    return $this->db->get();
+    }
+    public function getUserLimit()
+    {
+    $this->db->select('*');
+    $this->db->from('user');
+    $this->db->limit(10, 0);
+    return $this->db->get();
+    }
+   }
+   
 
 }
---
+++ application/views/gagal.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta charset="utf-8">
+ <meta http-equiv="X-UA-Compatible" content="IE=edge">
+ <meta name="viewport" content="width=device-width, initialscale=1, shrink-to-fit=no">
+ <meta name="description" content="">
+ <meta name="author" content="">
+ <title>Gagal Aktifasi Akun </title>
+ <!-- Custom fonts for this template-->
+ <link href="<?= base_url('assets/'); ?>vendor/fontawesomefree/css/all.min.css" rel="stylesheet" type="text/css">
+ <link
+href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,30
+0i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+ <!-- Custom styles for this template-->
+  <!-- Bootstrap core JavaScript -->
+ <script src="<?= base_url('assets/');
+?>vendor/jquery/jquery.min .js"></script>
+ <script src="<?= base_url('assets/');
+?>vendor/bootstrap/js/bootstrap.bundle.min.j s"></script>
+ <!-- Core plugin JavaScript -->
+ <script src="<?= base_url('assets/'); ?>vendor/jqueryeasing/jquery.easing.min.j s"></script>
+ <!-- Custom scripts for all pages-->
+ <script src="<?= base_url('assets/'); ?>js/sb-admin2.min.js"></script>
+ <script>
+ $('.alert-message').alert().delay(3500).slideUp('slow');
+ </script>
+</body>
+
+</html>
+
